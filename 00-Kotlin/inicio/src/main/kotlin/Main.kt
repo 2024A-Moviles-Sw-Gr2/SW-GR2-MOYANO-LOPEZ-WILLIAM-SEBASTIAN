622a4929bb8ecbebb94f879a4cbbd50b9326c5fc
WSebastianML <wsebastianmoyano@gmail.com>
2024-05-17 10:56:13 -0500

    filter y reduce en kotlin

--- 00-Kotlin/inicio/src/main/kotlin/Main.kt
+++ 00-Kotlin/inicio/src/main/kotlin/Main.kt
@@ -96,6 +96,49 @@ fun main(){
     println(respuestaMap)
     val respuestaMapDos = arregloDinamico.map { it + 15 }
     println(respuestaMapDos)
+
+    //Filter -> Filtrar el arreglo
+    // 1) Devollver una expresion (true o false)
+    // 2) Nuevo arreglo filtrado
+    val respuestaFilter: List<Int> = arregloDinamico
+        .filter { valorActual: Int ->
+            //Expresion o condicion
+            val mayoresACinco: Boolean = valorActual > 5
+            return@filter mayoresACinco
+        }
+
+    val respuestaFilterDos = arregloDinamico.filter { it <= 5 }
+    println(respuestaFilter)
+    println(respuestaFilterDos)
+
+    //OR AND
+    //OR -> ANY(Alguno cumple?)
+    //AND -> ALL(Todos cumplen?)
+    val respuestaAny: Boolean = arregloDinamico
+        .any{ valorActual: Int ->
+            return@any (valorActual > 5)
+        }
+    println(respuestaAny)//True
+    val respuestaAll: Boolean = arregloDinamico
+        .all{ valorActual: Int ->
+            return@all (valorActual > 5)
+        }
+    println(respuestaAll) //False
+
+    // REDUCE -> valor acumulado
+    // Valor acumulado = 0 (siempre empieza en 0 en Kotlin)
+    // [1,2,3,4,5] -> Acumular "SUMAR" estos valores del arreglo
+    // valorIteracion1 = valorEmpieza + 1 = 0 + 1 = 1 -> Iteracion 1
+    // valorIteracion2 = valorAcumuladoIteracion1 + 2 = 1 + 2 = 3 -> Iteracion 2
+    // valorIteracion3 = valorAcumuladoIteracion2 + 3 = 3 + 3 = 6 -> Iteracion 3
+    // valorIteracion4 = valorAcumuladoIteracion3 + 4 = 6 + 4 = 10 -> Iteracion 4
+    // valorIteracion5 = valorAcumuladoIteracion4 + 5 = 10 + 5 = 15 -> Iteracion 5
+    val respuestaReduce: Int = arregloDinamico
+        .reduce{ acumulado: Int, valorActual:Int ->
+            return@reduce (acumulado + valorActual) //Cambiar o usar la logica del negocio
+        }
+    println(respuestaReduce)
+    // return@reduce acumulado + (itemCarrito.cantidad * itemCarrito.precio)
 }
 
 fun imprimirNombre(nombre: String): Unit{
