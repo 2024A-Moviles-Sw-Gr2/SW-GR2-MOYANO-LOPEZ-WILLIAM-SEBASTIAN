72ac4f9c15d84a6cae6b88b8420b91c360726094
WSebastianML <wsebastianmoyano@gmail.com>
2024-07-04 09:03:09 -0500

    intents

--- 01-clases/app/src/main/java/com/example/a2024aswgr2wsml/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2wsml/MainActivity.kt
@@ -1,19 +1,71 @@
 package com.example.a2024aswgr2wsml
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    fun mostrarSnackBar(texto: String){
+        val snack = Snackbar.make(
+            findViewById(R.id.id_layout_main),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+    }
+
+    val callbackContenidoIntentExplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+            result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            if(result.data != null){
+                //Logica negocio
+                val data = result.data
+                mostrarSnackBar(
+                    "${data?.getStringExtra("nombreModificado")}"
+                )
+            }
+        }
+    }
+
+    val callbackContenidoIntentImplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+            result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            if(result.data != null){
+                //Logica negocio
+                if(result.data!!.data != null){
+                    var uri: Uri = result.data!!.data!!
+                    var cursor = contentResolver.query(
+                        uri, null, null, null, null, null
+                    )
+                    cursor?.moveToFirst()
+                    val indiceTelefono = cursor?.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    )
+                    val telefono = cursor?.getString(indiceTelefono!!)
+                    cursor?.close()
+                    mostrarSnackBar("Telefono $telefono")
+                }
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_ciclo_vida)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.id_layout_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -27,6 +79,35 @@ class MainActivity : AppCompatActivity() {
         val botonListView = findViewById<Button>(R.id.btn_ir_list_view)
         botonListView.setOnClickListener {
             irActividad(BListView::class.java)
+        }
+
+        val botonIntentImplicito = findViewById<Button>(
+            R.id.btn_ir_intent_implicito
+        )
+        botonIntentImplicito.setOnClickListener {
+            val intentConRespuesta = Intent(
+                Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+            )
+            callbackContenidoIntentImplicito.launch(intentConRespuesta)
+        }
+
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito
+        )
+        botonIntentExplicito.setOnClickListener {
+            val intentExplicito = Intent(
+                this,
+                CIntentExplicitoParametros::class.java
+            )
+            intentExplicito.putExtra("nombre", "Adrian")
+            intentExplicito.putExtra("apellido", "Eguez")
+            intentExplicito.putExtra("edad", 34)
+            intentExplicito.putExtra(
+                "entrenador",
+                BEntrenador(10, "Adrian", "Eguez")
+            )
+            callbackContenidoIntentExplicito.launch(intentExplicito)
         }
     }
 
